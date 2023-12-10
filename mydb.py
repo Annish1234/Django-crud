57e448890ad8f2ffbb8123bc6cef18ea8bf471d5
Annish1234 <annishjk01@gmail.com>
2023-12-10 10:44:16 +0530

    second commit

---
+++ mydb.py
@@ -0,0 +1,12 @@
+import mysql.connector
+
+# database  = mysql.connector.connect(
+#     host="localhost",
+#     user = "root",
+#     passwd = "root"
+# )
+
+# curserObject = database.cursor()
+
+# curserObject.execute("CREATE DATABASE dcrm")
+# print("all done!")
